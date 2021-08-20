dd3eddf11e04bbfb76618801b6d28bedfc2635b1
Shivam Kantival <shivam.kantival@mindtickle.com>
2021-08-20 12:16:46 +0530

    create  pwa

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const getActualRequestDurationInMilliseconds = (start) => {
+  const NS_PER_SEC = 1e9; //  convert to nanoseconds
+  const NS_TO_MS = 1e6; // convert to milliseconds
+  const diff = process.hrtime(start);
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+};
+
+let demoLogger = (req, res, next) => {
+  //middleware function
+  let current_datetime = new Date();
+  let formatted_date =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
+  let method = req.method;
+  let url = req.url;
+  let status = res.statusCode;
+  const start = process.hrtime();
+  const durationInMilliseconds = getActualRequestDurationInMilliseconds(start);
+  let log = `[${formatted_date}] ${method}:${url} ${status} ${durationInMilliseconds.toLocaleString()} ms`;
+  console.log(log);
+  next();
+};
+
+app.use(demoLogger);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(8011);
